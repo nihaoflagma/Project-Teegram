ae8e06ad3967957ee26937eb482ed162ca597159
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-05-11 19:09:54 +0300

    Update OtpVerificationService.java

--- Project Teegram/src/main/java/com/example/otp/service/OtpVerificationService.java
+++ Project Teegram/src/main/java/com/example/otp/service/OtpVerificationService.java
@@ -16,27 +16,27 @@ public class OtpVerificationService {
     @Autowired
     private OtpCodeRepository otpCodeRepository;
 
-    // Метод для верификации OTP
+    
     public boolean verifyOtp(String email, String code) {
-        // Ищем последний OTP код по email, не использованный
+        
         Optional<OtpCode> otpCodeOptional = otpCodeRepository.findByEmailAndCodeAndUsedFalse(email, code);
 
         if (otpCodeOptional.isPresent()) {
             OtpCode otpCode = otpCodeOptional.get();
 
-            // Проверка: код истек или нет
+            
             if (otpCode.getExpiresAt().isBefore(LocalDateTime.now())) {
                 log.warn("OTP код для email: {} истек!", email);
                 return false;
             }
 
-            // Проверка: был ли использован код
+            
             if (otpCode.isUsed()) {
                 log.warn("OTP код для email: {} уже использован!", email);
                 return false;
             }
 
-            // Если код правильный, помечаем как использованный
+            
             otpCode.setUsed(true);
             otpCodeRepository.save(otpCode);
 
