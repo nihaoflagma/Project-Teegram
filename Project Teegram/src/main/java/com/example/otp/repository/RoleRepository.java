d9888f921f9370669dbb301e757e57a1d0e075e9
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-05-11 19:08:25 +0300

    Update RoleRepository.java

--- Project Teegram/src/main/java/com/example/otp/repository/RoleRepository.java
+++ Project Teegram/src/main/java/com/example/otp/repository/RoleRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Role findByName(String name); // Найти роль по имени
+    Role findByName(String name); 
 }
